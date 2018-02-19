ea603c3e1ad8f1404065408a1c578f123f45a8bd
lucifyer <gawasvishnu26@gmail.com>
2018-02-20 01:18:02 +0530

    removed todo, instead adding issues

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 
-	$con = mysqli_connect('localhost','root','incorrect','hackathon1.1');
+	$con = mysqli_connect('localhost','root','incorrect','hackathon1_1');
 
 	if(mysqli_connect_error($con))
 	{
